df08db777eb5151260652542499366c62c16d99f
Jbraham99 <braham.joseph15@gmail.com>
2023-12-17 22:41:30 -0800

    updated projects page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -15,27 +15,27 @@ function About() {
             <div>
                 <h3 className="text-center pt-4">Experience</h3>
                 <div className="row pb-4">
-                    <div className="col">
-                        <h4 className="text-center">Thinkful | Apprentice</h4>
+                    <div className="col-lg-6">
+                        <h4 className="text-center pb-1">Thinkful | Student</h4>
                         <img className="rounded mx-auto d-block" src="https://media1.tenor.com/m/5ry-200hErMAAAAd/hacker-hacker-man.gif" height={300} width={300} />
                     </div>
-                    <p className="col">I immersed myself in acquiring industry best practices and adhering to software development standards, concentrating on a comprehensive skill set that encompassed Javascript, HTML5, CSS3, React Native, Node.js, PostgreSQL, RESTful APIs, as well as algorithms and data structures. Throughout this journey, I actively engaged in the development and deployment of mobile-first applications, constantly expanding my language and framework proficiency. To enhance my expertise, I dedicated several hours each week to collaborative efforts with senior web developers, fostering a mentor-student relationship that provided invaluable insights and knowledge. Additionally, I gained a solid understanding of backend concepts, incorporating Express, PostgreSQL, data structures and algorithms (DSA), and object-oriented programming (OOP) into my skill repertoire.</p>
+                    <p className="col pt-4">I immersed myself in acquiring industry best practices and adhering to software development standards, concentrating on a comprehensive skill set that encompassed Javascript, HTML5, CSS3, React Native, Node.js, PostgreSQL, RESTful APIs, as well as algorithms and data structures. Throughout this journey, I actively engaged in the development and deployment of mobile-first applications, constantly expanding my language and framework proficiency. To enhance my expertise, I dedicated several hours each week to collaborative efforts with senior web developers, fostering a mentor-student relationship that provided invaluable insights and knowledge. Additionally, I gained a solid understanding of backend concepts, incorporating Express, PostgreSQL, data structures and algorithms (DSA), and object-oriented programming (OOP) into my skill repertoire.</p>
                 </div>
                 <hr></hr>
                 <div className="row pb-4">
-                    <div className="col">
-                        <h4 className="text-center">T-Mobile</h4>
+                    <div className="col-lg-6">
+                        <h4 className="text-center pb-1">T-Mobile | Mobile Expert</h4>
                         <img className="rounded mx-auto d-block" src="https://media2.giphy.com/media/XPghyZNehTVVG3JLlS/giphy.gif" height={300} width={300} />
                     </div>
-                    <p className="col">In my role at T-Mobile, I've excelled in over-indexing the incorporation of value-added features, achieving an impressive average increase of 92%. Additionally, I successfully led and grew a dynamic sales team comprised of 10 individuals, consistently surpassing daily goals with a remarkable 125% performance. Through strategic initiatives, I played a pivotal role in driving up customer traffic by 5-10% during downtimes, showcasing my ability to adapt and enhance business outcomes. Furthermore, I fostered a collaborative work environment, instilling a culture of teamwork and accountability that contributed to the overall success of the team.</p>
+                    <p className="col pt-4 align-midde">In my role at T-Mobile, I've excelled in over-indexing the incorporation of value-added features, achieving an impressive average increase of 92%. Additionally, I successfully led and grew a dynamic sales team comprised of 10 individuals, consistently surpassing daily goals with a remarkable 125% performance. Through strategic initiatives, I played a pivotal role in driving up customer traffic by 5-10% during downtimes, showcasing my ability to adapt and enhance business outcomes. Furthermore, I fostered a collaborative work environment, instilling a culture of teamwork and accountability that contributed to the overall success of the team.</p>
                 </div>
                 <hr></hr>
                 <div className="row pt-4 pb-4">
-                    <div className="col">
-                        <h4 className="text-center">J.Crew Factory</h4>
+                    <div className="col-lg-6">
+                        <h4 className="text-center pb-1">J.Crew Factory | Sales Associate</h4>
                         <img className="rounded mx-auto d-block" src="https://media3.giphy.com/media/gCXBEvSJDHVoyRPQeX/giphy.gif?cid=ecf05e47nt0t2rnpr6cdno5giqbm2k7bbatdp44yzcmspjzs&ep=v1_gifs_search&rid=giphy.gif&ct=g" height={300} width={300} />
                     </div>
-                    <p className="col">I played a pivotal role in boosting sales by consistently surpassing both selling and service expectations. To enhance customer engagement, I meticulously developed an itemized inventory of products tailored to individual preferences, providing a personalized and seamless shopping experience. Additionally, I actively collaborated with the store director, contributing to the development of innovative strategies to meet and exceed sales goals. Through a combination of strategic thinking and a customer-centric approach, I was able to contribute to the overall success of the sales team and the achievement of business objectives.</p>
+                    <p className="col pt-4">I played a pivotal role in boosting sales by consistently surpassing both selling and service expectations. To enhance customer engagement, I meticulously developed an itemized inventory of products tailored to individual preferences, providing a personalized and seamless shopping experience. Additionally, I actively collaborated with the store director, contributing to the development of innovative strategies to meet and exceed sales goals. Through a combination of strategic thinking and a customer-centric approach, I was able to contribute to the overall success of the sales team and the achievement of business objectives.</p>
                 </div>
             </div>
         </div>
--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -2,12 +2,30 @@
 
 function Projects() {
     return (
-        <div>
-            <h2 className="text-center pt-3">Projects</h2>
-            <div className="col-sm-12 col-md-6">
-            <h5 className="text-center">Reservation App</h5>
-            <p className="bg-dark">.</p>
-            <img src="" />
+        <div className="container">
+            <h2 className="text-center pt-3 pb-3">Projects</h2>
+            <div className="row">
+                <div className="col-lg-6">
+                    <h4 className="text-center pb-1">Reservation App</h4>
+                    <img className="rounded mx-auto d-block" src="https://media0.giphy.com/media/39orZxS2uTsLxz7nb8/giphy.gif?cid=ecf05e47crfpvh85yqsvhkg78agaew3qcztzc14mm4zmatk1&ep=v1_gifs_search&rid=giphy.gif&ct=g" height={300} width={300} />
+                    <div className="d-flex justify-content-around pt-4">
+                        <a className="" href="https://customer-welcome.onrender.com/dashboard">Try App Here!</a>
+                        <a className="" href="https://github.com/Jbraham99/starter-restaurant-reservation.git">GitHub Repo</a>
+                    </div>
+                </div>
+                <p className="col-lg-6 pt-4">The reservation management application provides users with a comprehensive tool for handling customer reservations efficiently. Crafted with an emphasis on scalability and sustainability, the application is built using reusable components and leverages front-end libraries to facilitate seamless integration and future adaptability. The backend architecture is constructed using NodeJS, Express, and ElephantSQL, ensuring robust functionality and a reliable foundation. The development process also prioritized the establishment of an organized data architecture to enhance data management. The application implements all CRUD (Create, Read, Update, Delete) operations through a RESTful API structure, offering a versatile and user-friendly interface for effectively managing reservations.</p>
+            </div>
+            <hr className=""></hr>
+            <div className="row">
+                <div className="col-lg-6">
+                    <h4 className="text-center pb-1">Recipe Tracker</h4>
+                    <img className="rounded mx-auto d-block" src="https://media0.giphy.com/media/N23cG6apipMmQ/giphy.gif?cid=ecf05e47ov0bk62sv2ahopfyynk6jx0o24hquerom25lmk2o&ep=v1_gifs_search&rid=giphy.gif&ct=g" height={300} width={300} />
+                    <div className="d-flex justify-content-around pt-4">
+                        <a className="" href="https://recipe-tracker-app-oc4u.onrender.com">Try App Here!</a>
+                        <a className="" href="https://github.com/Jbraham99/RecipeTracker">GitHub Repo</a>
+                    </div>
+                </div>
+                <p className="col-lg-6 pt-4">The digital recipe book application is designed to empower users in managing their favorite recipes effortlessly. It offers a user-friendly platform for creating, reading, and deleting recipes, providing a seamless experience for culinary enthusiasts. The application is aesthetically styled using Bootstrap and custom CSS, ensuring a visually appealing and intuitive interface. Notably, a key feature has been implemented to expedite the process of recording favorite recipes, boasting a remarkable 185% increase in speed compared to manual writing. This enhancement not only streamlines the user experience but also encourages the swift and convenient documentation of cherished culinary creations.</p>
             </div>
         </div>
     )
